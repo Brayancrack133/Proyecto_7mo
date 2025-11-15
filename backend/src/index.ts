5c9038609619a91fee5d8712508c512dd6fc7efe
Alex-Apaza <apazaj@gmail.com>
2025-11-15 00:31:19 -0400

    BackendGestionuser

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import "./config/db.js"; // SOLO importar para inicializar conexión
+import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 3000;
 
